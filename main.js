34ad018bb22f886c04e9ac016bdd945c034bf46d
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2022-07-19 16:40:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 var SpeechRecognition = window.webkitSpeechRecogniton;
+
 var recognition = new SpeechRecognition();
+
 function start(){
     document.getElementById("textbox").innerHTML = "";
     recognition.start();
 }
 recognition.onresult = function(event){
     console.log(event);
+
     var Content = event.result[0][0].transcript;
+    
+    document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    document.getElementById("textbox").innerHTML= Content;
-    speak();
+    if (Content == "take my selfie") {
+        console.log("Taking selfie---");
+        speak();
+    }
 }
 function speak(){
     var synth = window.SpeechSynthesis;
-    speak_data = document.getElementById("textbox").innerHTML;
+    speak_data = "Taking your selfie in 5 seconds.";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    set_timeout(function(){
+        take_snapshot();
+        save();
+    },
+    5000);
 }
 
 Webcam.set({
@@ -26,3 +38,15 @@ Webcam.set({
     png_quality : 90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="selfie_image" src="'+ data_uri +'">';
+    });
+}
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selife_image").src;
+    link.href = image;
+    link.click();
+}
